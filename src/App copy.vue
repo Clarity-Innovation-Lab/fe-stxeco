227d3c6af8d2f6f0332624e08a014e9a8f42e9a1
Mike Cohen <mjoecohen@gmail.com>
2022-07-09 18:37:38 +0100

    Fix for build issues 1. removed node-sass 2. found correct polyfill for Buffer with webpack 5

---
+++ src/App copy.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    id="app"
+    :style="'z-index: -40; min-height: 90vh; background-size: contain; background-image: url(' + getPixelBackground + ')'"
+  >
+    <div
+      v-if="loaded"
+      class="wrapper"
+    >
+      <div class="bg-danger py-3 text-center under-construction">
+        UNDER CONSTRUCTION - <a
+          href="https://discord.com/channels/621759717756370964/971037457661444156"
+          target="_blank"
+        >contributions welcome via discord!</a>
+      </div>
+      <RouterView
+        name="header"
+        class="bg-dark"
+      />
+      <RouterView
+        name="daoheader"
+        class="bg-warning text-white"
+      />
+      <RouterView
+        id="page"
+        class="mt-5"
+      />
+      <div class="push" />
+    </div>
+    <footer class="footer">
+      <RouterView
+        class=""
+        name="footer"
+      />
+    </footer>
+    <notifications
+      :duration="4000"
+      classes="r-notifs"
+      position="bottom left"
+      width="70%"
+    />
+  </div>
+</template>
+<script>
+import { APP_CONSTANTS } from '@/app-constants'
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      loaded: true
+    }
+  },
+  computed: {
+    getPixelBackground () {
+      return this.$store.getters[APP_CONSTANTS.KEY_PIXEL_BACKGROUND]
+    }
+  },
+  beforeCreate () {
+    this.$store.dispatch('initDaoApplication').then(() => {
+      this.loaded = true
+    })
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
